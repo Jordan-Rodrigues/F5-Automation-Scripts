f0e58523ede936ef9108f0c2cb555d3409a4131d
Jordan Rodrigues <jrodrigues0801@gmail.com>
2019-05-23 15:55:56 -0500

    Removing Sensitive File

--- testScript.py
+++
@@ -1,6 +0,0 @@
-from f5.bigip import ManagementRoot
-
-mgmt = ManagementRoot("10.80.191.92","admin","cC50KXf1=rmOxdp")
-pools = mgmt.tm.ltm.pools
-pool = mgmt.tm.ltm.pools.pool
-print(mgmt.tm.ltm.pools.pool.exists(name="Pool-1", parition="Common"))
